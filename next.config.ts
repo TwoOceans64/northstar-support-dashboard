b819eab5ec9b51fd06b19264185ec6d5f0fc73f0
jmbroM11 Stealth <jmbrowers93@gmail.com>
2026-08-15 18:14:50 +0200

    Fix turbopack.root path to frontend/my-app

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 const nextConfig: NextConfig = {
   turbopack: {
     // Use an absolute path so Vercel doesn’t duplicate folders
-    root: path.resolve(__dirname, "Frontend/my-app"),
+    root: path.resolve(__dirname, "frontend/my-app"),
   },
 };
 
