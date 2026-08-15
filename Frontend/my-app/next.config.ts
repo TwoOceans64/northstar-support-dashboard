4c7bd1046b021bbfbff75dd17823df9327c56152
jmbroM11 Stealth <jmbrowers93@gmail.com>
2026-08-15 15:51:32 +0200

    Set turbopack.root to Frontend/my-app for Vercel build

--- Frontend/my-app/next.config.ts
+++ Frontend/my-app/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: "./Frontend/my-app",
+  },
 };
 
 export default nextConfig;
